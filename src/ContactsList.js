21b7017c182bd62b20c51001ad8ba393cec517ad
Steven Jay <stevenjasionowicz@gmail.com>
2016-03-17 00:14:07 +1300

    Exporting and importing a child component

---
+++ src/ContactsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ContactsList extends React.Component {
+	render() {
+		return (
+			<ul>
+				<li>Steven 022 222 2222</li>
+			</ul>
+			)
+	}
+}
+
+export default ContactsList;
